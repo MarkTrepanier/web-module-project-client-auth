883f41e7acfd1dbc973d77a686177226ebf6118d
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-12 17:24:02 -0400

    more work on login

--- friends/src/components/login.js
+++ friends/src/components/login.js
@@ -1,16 +1,42 @@
-import React from "react";
+import React, {useState}from "react";
+
 const Login = () => {
+    const initialInput = {
+        username:'',
+        password:''
+    }
+    const [isLoading, setIsLoading] = useState(false)
+    const [input, setInput] = useState(initialInput)
+
+    const handleChange = e => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return(
         <div>
             <form>
                 <div>
                     <label>UserName
-                        <input type='text' placeholder='Craig Robinson'></input>
+                        <input 
+                        type='text' 
+                        name='username' 
+                        placeholder='Craig Robinson'
+                        value={input.username}
+                        onChange={handleChange}
+                        ></input>
                     </label>
                 </div>   
                 <div>
                     <label>Password
-                        <input type='password'></input>
+                        <input 
+                        type='password' 
+                        name='password' 
+                        value={input.password}
+                        onChange={handleChange}
+                        ></input>
                     </label>
                 </div>  
                 <button>submit</button> 

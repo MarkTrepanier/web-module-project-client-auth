0b4b6aa582f611c3c28f1533927adee8d22a34e6
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-12 21:41:27 -0400

    strings are my bane

--- friends/src/components/friendsList.js
+++ friends/src/components/friendsList.js
@@ -7,16 +7,17 @@ class FriendsList extends React.Component{
     }
     componentDidMount(){
         axiosWithAuth()
-            .get('https://localhost:5000/api/friends')
-            .then(res=>{
-                console.log('fetch succesful')
-                console.log(res)
-            })
-            .catch(er=>{
-                console.log('error with friends fetch')
-                console.log(er)
-            })
+            .get('http://localhost:5000/api/friends')
+                .then(res=>{
+                    console.log('fetch succesful')
+                    console.log(res)
+                })
+                .catch(er=>{
+                    console.log('error with friends fetch')
+                    console.error(er)
+                })
     }
+
     render(){
         return(
             <div>

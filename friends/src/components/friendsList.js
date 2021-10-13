bf8718ec077686419e9eed92cc6c52cb3a4c0b63
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-10-12 22:44:46 -0400

    friends rendered

--- friends/src/components/friendsList.js
+++ friends/src/components/friendsList.js
@@ -10,7 +10,9 @@ class FriendsList extends React.Component{
             .get('http://localhost:5000/api/friends')
                 .then(res=>{
                     console.log('fetch succesful')
-                    console.log(res)
+                    this.setState({
+                        friends: res.data
+                    })
                 })
                 .catch(er=>{
                     console.log('error with friends fetch')
@@ -23,7 +25,9 @@ class FriendsList extends React.Component{
             <div>
                 <h1>you made it to the friend's list!!</h1>
                 <div className='list'>
-                    
+                    {this.state.friends ? this.state.friends.map(friend=>{
+                        return <h2 key={friend.id}>{friend.name}</h2>
+                    }):<></>}
                 </div>
             </div>
         )
